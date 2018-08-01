1fab0be4bbef12e3ffcb79b54b2a474879dc3a81
Peter Georgeson <peter@supernifty.org>
2018-08-01 19:17:07 +1000

    fix placeholders

--- src/main/java/org/bionitio/package-info.java
+++ src/main/java/org/bionitio/package-info.java
@@ -1,9 +1,9 @@
 /**
 * The bionitio package generates simple FASTA file statistics.
 *
-* Copyright   : (c) BIONITIO_AUTHOR, BIONITIO_DATE 
-* License     : BIONITIO_LICENSE 
-* Maintainer  : BIONITIO_EMAIL 
+* Copyright   : (c) BIONITIO_AUTHOR, BIONITIO_DATE
+* License     : BIONITIO_LICENSE
+* Maintainer  : BIONITIO_EMAIL
 * Portability : POSIX
 *
 */
